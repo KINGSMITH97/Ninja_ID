bf7f380d4079cf4751bdc5b31f9f3f5f0e4df06c
KINGSMITH97 <kingsleyappiah2143@gmail.com>
2022-11-21 02:31:11 +0000

    first_commit

---
+++ android/app/src/main/kotlin/com/example/ninja_id_proj/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ninja_id_proj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Ninja ID"),
+          backgroundColor: Colors.grey[850],
+          elevation: 0.0,
+        ),
+      ),
+    ),
+  );
+}
